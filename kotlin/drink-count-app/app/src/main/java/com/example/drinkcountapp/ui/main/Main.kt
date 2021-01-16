0cf157b89981caee135476d07e18cc90780cc26f
Georg_Seng <georg.sengstbratl@gmail.com>
2021-01-16 16:30:15 +0100

    Add changes

--- kotlin/drink-count-app/app/src/main/java/com/example/drinkcountapp/ui/main/Main.kt
+++ kotlin/drink-count-app/app/src/main/java/com/example/drinkcountapp/ui/main/Main.kt
@@ -3,12 +3,15 @@ package com.example.drinkcountapp.ui.main
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.drinkcountapp.R
+import kotlinx.coroutines.flow.combine
 
 class main : Fragment() {
+
 
     companion object {
         fun newInstance() = main()
@@ -20,6 +23,10 @@ class main : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val binding: FragmentMainBinder = DataBindingUtil.inflate(inflater, R.layout.fragment_main,
+        container,false)
+
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
@@ -27,6 +34,9 @@ class main : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+        val binding: UserBinding = DataBindingUtil.setContentView(this, R.layout.user)
+
+        binding. =
     }
 
 }
